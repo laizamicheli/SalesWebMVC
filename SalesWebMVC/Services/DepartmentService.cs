be482b0929c15b82eef887f007b3da924b294f12
laizamicheli <74686128+laizamicheli@users.noreply.github.com>
2022-05-15 18:19:33 -0300

    Task async await

--- SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -3,6 +3,8 @@ using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace SalesWebMVC.Services
 {
@@ -15,9 +17,9 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-        public List <Department> FindAll()
+        public async Task<List <Department>> FindAllAsinc()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
